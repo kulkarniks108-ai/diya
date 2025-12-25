462da0e1b513080438590a6a1a8495464bccd314
ishankulkarni13 <ishankulkarni13@gmail.com>
2025-12-25 21:48:09 +0530

    fix(firebase): fix account getting logged out after closing the app

--- expoApp/config/firebase.ts
+++ expoApp/config/firebase.ts
@@ -1,13 +1,13 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { initializeApp } from "firebase/app";
-import { inMemoryPersistence, initializeAuth } from "firebase/auth";
+import {
+  getReactNativePersistence,
+  initializeAuth,
+} from "firebase/auth";
 
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from 'firebase/storage';
+import { getStorage } from "firebase/storage";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBiJxJDxfdHAC_YHgVXeoq2lPV5Tr5k-eg",
   authDomain: "ndeye-740a8.firebaseapp.com",
@@ -17,13 +17,11 @@ const firebaseConfig = {
   appId: "1:328019830392:web:62f2a6c33d48b1d2c16f8d"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export const auth = initializeAuth(app, {
-  persistence: inMemoryPersistence,
+  persistence: getReactNativePersistence(AsyncStorage),
 });
+
 export const db = getFirestore(app);
 export const storage = getStorage(app);
-
-
-
