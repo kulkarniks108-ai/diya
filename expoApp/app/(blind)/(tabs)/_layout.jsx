8dcb15b2e557b06c29dd5ef7c22deafa09336add
ishankulkarni13 <ishankulkarni13@gmail.com>
2025-12-28 23:43:43 +0530

    (blind routes): deleted or moved unnecessary routes

--- expoApp/app/(blind)/(tabs)/_layout.jsx
+++ expoApp/app/(blind)/(tabs)/_layout.jsx
@@ -27,7 +27,7 @@ export default function BlindTabLayout() {
       }}
     >
       <Tabs.Screen
-        name="main"
+        name="index"
         options={{
           title: "Main",
           tabBarIcon: ({ color, size }) => (
@@ -53,33 +53,7 @@ export default function BlindTabLayout() {
           ),
         }}
       />
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Home",
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="home-outline" size={size} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="imageRec"
-        options={{
-          title: "Image rec",
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="add-circle-outline" size={size} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: "Profile",
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="person-outline" size={size} color={color} />
-          ),
-        }}
-      />
+   
     </Tabs>
   );
 }
