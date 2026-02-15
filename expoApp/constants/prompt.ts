ffd364145e1a7ec3f6803a96b5c05dc70f1efcc4
ishankulkarni13 <ishankulkarni13@gmail.com>
2026-02-15 17:12:03 +0530

    chor: save progress of Phase I

--- expoApp/constants/prompt.ts
+++ expoApp/constants/prompt.ts
@@ -1,6 +1,8 @@
 export const PROMPTS = {
-    imageAssist: {
-        DescribeInShortFocus: "Describe the surroundings and warn about obstacles in short focus on what is on the centre and read the text of the object in front of you has a text which is important to be read",
-        DescribeInLongDetail: "Describe the surroundings and warn about obstacles in long detail",
-    }
-}
+  imageAssist: {
+    DescribeInShortFocus:
+      "Describe the surroundings and warn about obstacles in short focus on what is on the centre and read the text of the object in front of you has a text which is important to be read and focus on centre only and keep this short and crisp",
+    DescribeInLongDetail:
+      "Describe the surroundings and warn about obstacles in long detail",
+  },
+};
